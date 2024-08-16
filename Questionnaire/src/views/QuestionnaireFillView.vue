6c3d7aef114cac832fc870057e587f6b4c3cb39e
TieZhuGieGie <dcx1378832571@163.com>
2024-08-16 13:38:54 +0800

    TieZhu

--- Questionnaire/src/views/QuestionnaireFillView.vue
+++ Questionnaire/src/views/QuestionnaireFillView.vue
@@ -60,7 +60,7 @@
             <van-checkbox-group v-model=" questionList[index-1].Answer" v-for="index2 in questionList[index-1].optionCnt"  checked-color="#0283EF" :disabled="flag">
                 <br/>
                 <van-checkbox :name="questionList[index-1].optionList[index2-1].optionId" shape="square" :label-disabled=true>
-                    <div>
+                    <div  :disabled="true">
                       {{ questionList[index-1].optionList[index2-1].content }}
                     </div>
                 </van-checkbox>
